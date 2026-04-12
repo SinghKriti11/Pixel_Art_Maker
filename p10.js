97497d84b8b688ec353f6a0753dcf248f8e5aea8
Kritii <kritikumari53782@gmail.com>
2026-04-13 00:22:50 +0530

    added

--- p10.js
+++ p10.js
@@ -1,3 +1,7 @@
 let container = document.querySelector(".container");
 let gridButton = document.getElementById("submit-grid");
 let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
